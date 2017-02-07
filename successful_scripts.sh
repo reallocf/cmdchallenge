387a72ca7e2346025c8b187184432131f9c09dea
Charlie Summers <charliegsummers@gmail.com>
2017-02-06 18:14:27 -0800

    Fixed small styling typo

--- successful_scripts.sh
+++ successful_scripts.sh
@@ -141,7 +141,7 @@ awk '!seen[$0]++' faces.txt
 # 29. corrupted_text/
 # The following excerpt from War and Peace is saved to the file 'war_and_peace.txt':
 # She is betraying us! Russia alone must save Europe. Our gracious sovereign recognizes his high vocation and will be true to it. That is the one thing I have faith in! Our good and wonderful sovereign has to perform the noblest role on earth, and he is so virtuous and noble that God will not forsake him. He will fulfill his vocation and crush the hydra of revolution, which has become more terrible than ever in the person of this murderer and villain!
-#The file however has been corrupted, there are random '!' marks inserted throughout. Print the original text.
+# The file however has been corrupted, there are random '!' marks inserted throughout. Print the original text.
 
 cat war_and_peace.txt | tr -s ! | sed "s/ !/ /g" | sed "s/!\([^ ]\)/\1/g" | sed "s/! \([a-z]\)/ \1/g" | sed "s/\([[:punct:]]\)!/\1/g"
 
